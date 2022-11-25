84c8971e9cb202b92917e659683c5ea2e33a1507
zainabhusain227 <zainabhusain227@gmail.com>
2022-11-24 21:47:16 -0500

    working streamlines and lnh

--- app.py
+++ app.py
@@ -47,6 +47,9 @@ def get_img():
     op_percent=request.args.get('StreamlineTransparency')
     op= int(op_percent)/100
 
+    thresh_percent=request.args.get('LNH_Threshold')
+    lnh_thresh= int(thresh_percent)/100
+
     # bool types
     show_edges = request.args.get('show_edges') == 'true'
     lighting = request.args.get('lighting') == 'true'
@@ -75,7 +78,7 @@ def get_img():
         #print(request.args.get('show_edges'))
         ####this is the slider parameter to adjust lnh thresholding (values 0 to 1)#####
         #lnh_thresh_percent= request.form["LNHThreshValue"]
-        lnh_thresh= 0.7 #lnh_thresh_percent/100
+        #lnh_thresh= 0.7 #lnh_thresh_percent/100
         ####this is the slider parameter to adjust streamline opacity (values 0 to 1)#####
         #streamline_op_percent= request.args.get('rangeValue')
         #int_stream= int(streamline_op_percent)
@@ -112,8 +115,8 @@ def get_img():
     #     show_edges=show_edges,
     #     color=color,
     # )
-    p.add_mesh(mesh, style=style, lighting=lighting, opacity=op)
-    '''
+    #p.add_mesh(mesh, style=style, lighting=lighting, opacity=op)
+    
     lnh_data.set_active_scalars('LNH')
     #lnh ranges from -1 to 1, we must calculate the negative lnh threshold as well (is if threshold is 0.5 we will plot values above 0.5 and below -0.5)
     neg_lnh_thresh= float (0) - float(lnh_thresh)
@@ -123,11 +126,11 @@ def get_img():
     neg_lnh= lnh_data.threshold(neg_lnh_thresh,invert=True)
 
     #plot the streamlines mesh
-    p.add_mesh(streamline_data, name="Streamlines", color='purple',opacity=streamline_op)
+    p.add_mesh(streamline_data, name="Streamlines", color='purple',opacity=op)
 
     #plot the 2 meshes
     p.add_mesh(pos_lnh, name="lnh_pos", color='red', opacity=1)
-    p.add_mesh(neg_lnh, name="lnh_neg", color='blue',opacity=1) '''
+    p.add_mesh(neg_lnh, name="lnh_neg", color='blue',opacity=1) 
 
     if anti_aliasing:
         p.enable_anti_aliasing()
